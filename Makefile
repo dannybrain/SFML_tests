e290e6628e750dc3a7e3691839c694d361bcdff4
Daniel Biehle <dannybrain@dannybrain.org>
2017-12-31 08:24:59 -0500

    Modified Makefile to support new Snake files main cleared/simplified

--- Makefile
+++ Makefile
@@ -1,8 +1,23 @@
+CC=c++
 CPPFLAGS=-std=c++11 -g
 LDFLAGS=-L/usr/local/lib -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -framework Foundation
+SOURCES=main.cpp ResourcePath.mm Game.cpp Snake.cpp Window.cpp
+OBJECTS=main.o ResourcePath.o Game.o Snake.o Window.o
+EXECUTABLE=game
 
-game: main.o ResourcePath.o
-	c++ $(CPPFLAGS) $(LDFLAGS) main.o ResourcePath.o -o game
+all: $(EXECUTABLE) $(OBJECTS)
 
-main.o: main.cpp
-	c++ $(CPPFLAGS) -c main.cpp
+game:$(OBJECTS)
+	$(CC) $(CPPFLAGS) $(LDFLAGS) $^ -o $@
+
+main.o:main.cpp
+	$(CC) $(CPPFLAGS) -c $<
+
+ResourcePath.o: ResourcePath.mm
+	$(CC) $(CPPFLAGS) $(LDFLAGS) -c $< -o $@
+
+clean:
+	rm game $(OBJECTS)
+
+
+.PHONY: all clean
--- main.cpp
+++ main.cpp
@@ -1,88 +1,23 @@
+#include <iostream>
+
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
-// Here is a small helper for you! Have a look.
-#include "ResourcePath.hpp"
+#include "Game.hpp"
 
 int main(int, char const**)
 {
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    Game game;
 
-    // Set the Icon
-    sf::Image icon;
-    if (!icon.loadFromFile(resourcePath() + "icon.png")) {
-        return EXIT_FAILURE;
+    while (!game.getWindow()->isDone()) {
+        game.handleInput();
+        game.update();
+        game.render();
+        //std::cout << game.getDelta().asMicroseconds() << std::endl;
+        game.restartClock();
+        if (game.getCumulativeDelta().asSeconds() >= 10)
+            std::cout << game.getCumulativeDelta().asSeconds() << std::endl;
     }
-    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
-
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile(resourcePath() + "cute_image.jpg")) {
-        return EXIT_FAILURE;
-    }
-    sf::Sprite sprite(texture);
     
-    sf::Texture myicon;
-    myicon.loadFromFile(resourcePath() + "icon.png");
-    sf::Sprite sprite_icon(myicon);
-    sprite_icon.setPosition(window.getSize().x / 2, window.getSize().y / 2);
-
-    // Create a graphical text to display
-    sf::Font font;
-    if (!font.loadFromFile(resourcePath() + "sansation.ttf")) {
-        return EXIT_FAILURE;
-    }
-    sf::Text text("Hello SFML", font, 50);
-    text.setFillColor(sf::Color::Black);
-
-    // draw Rectangle
-    sf::RectangleShape rectangle(sf::Vector2f(320.0, 200.0));
-    rectangle.setFillColor(sf::Color::Red);
-    rectangle.setPosition(50, 50);
-    
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile(resourcePath() + "nice_music.ogg")) {
-        return EXIT_FAILURE;
-    }
-
-    // Play the music
-    music.play();
-
-    // Start the game loop
-    while (window.isOpen())
-    {
-        // Process events
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // Close window: exit
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-
-            // Escape pressed: exit
-            if (event.type == sf::Event::KeyPressed) {
-                text.setString(std::to_string(event.key.code));
-                
-            }
-        }
-
-        // Clear screen
-        window.clear();
-
-        // Draw the sprite
-        window.draw(sprite);
-       // window.draw(rectangle);
-        window.draw(sprite_icon);
-
-        // Draw the string
-        window.draw(text);
-
-        // Update the window
-        window.display();
-    }
-
     return EXIT_SUCCESS;
 }
