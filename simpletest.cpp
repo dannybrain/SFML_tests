739c19d4a353c7d79125449c8b2d21aaca88341d
Daniel Biehle <dannybrain@dannybrain.org>
2018-01-28 12:22:10 -0500

    new tests

--- simpletest.cpp
+++ simpletest.cpp
@@ -17,5 +17,10 @@ int main(int argc, char** argv) {
     MaClass cool;
     std::cout << "hehe = " << cool.getNumber() << std::endl;
 
+
+    std::pair<int,int> mytuple(10, 20);
+
+    std::cout << std::get<0>(mytuple) << std::endl;
+
     exit(0);
 }
